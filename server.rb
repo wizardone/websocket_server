ce81d9a26e2444794a34e012da6927d406693327
Stefan Slaveykov <wizard.oneandonly@gmail.com>
2016-08-08 21:28:00 +0300

    add setup and example

--- server.rb
+++ server.rb
@@ -1,5 +1,25 @@
 require 'faye/websocket'
 
 Server = ->(env) do
-  [200, {}, ['test me']]
+  if Faye::WebSocket.websocket?(env)
+    socket = Faye::WebSocket.new(env)
+
+    socket.on(:open) do |event|
+      puts 'Opening socket connection...'
+    end
+
+    socket.on(:message) do |event|
+      puts 'Receiveing data: '
+      puts event.data
+    end
+
+    socket.on(:close) do |event|
+      puts 'Closing socket connection...'
+      socket = nil
+    end
+
+    socket.rack_response
+  else
+    [200, {'Content-Type' => 'text/plain'}, ['Not meant to be here']]
+  end
 end
